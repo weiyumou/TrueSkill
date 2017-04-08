774ed1311774e9623bc8adb365bcc783890bd797
weiyumou <weiy0007@e.ntu.edu.sg>
2017-04-08 16:27:00 +0800

    added overtime

--- trueskill_algo.py
+++ trueskill_algo.py
@@ -17,7 +17,7 @@ class TrueSkillEvaluator:
     def startEvaluation(self):
         import trueskill
         self.home_adv_adjust()
-        trueskill.setup(draw_probability = 0.0)
+        trueskill.setup(draw_probability = self.draw_prob())
         team_rating = dict(zip(self.teams, [trueskill.global_env().create_rating()] * len(self.teams)))
         
         for match in self.regular_matches:
@@ -29,7 +29,8 @@ class TrueSkillEvaluator:
             if wteam in team_rating and lteam in team_rating:
                 if wscore < lscore:
                     wteam, lteam = lteam, wteam
-                team_rating[wteam], team_rating[lteam] = trueskill.rate_1vs1(team_rating[wteam], team_rating[lteam])
+                team_rating[wteam], team_rating[lteam] = trueskill.rate_1vs1(team_rating[wteam], \
+                    team_rating[lteam], drawn = self.is_equal_score(wscore, lscore))
                 
         probs = []
         for i in range(0, len(teams)):
@@ -38,6 +39,18 @@ class TrueSkillEvaluator:
                 probs.append(prob)
 
         return probs
+
+    def is_equal_score(self, wscore, lscore):
+        return abs(wscore - lscore) <= 1e-7
+
+    def draw_prob(self):
+        num_draw = 0
+        for match in self.regular_matches:
+            wscore = match[self.regular_titles.index('Wscore')]
+            lscore = match[self.regular_titles.index('Lscore')]
+            if self.is_equal_score(wscore, lscore):
+                num_draw += 1
+        return num_draw / len(self.regular_matches)
 
     def win_probability(self, rating_a, rating_b):
         from trueskill import BETA
@@ -48,6 +61,17 @@ class TrueSkillEvaluator:
                      + pow(rating_b.sigma, 2))
         return cdf(delta_mu / denom)
 
+
+class NoOvertime(TrueSkillEvaluator):
+    def __init__(self, eval_teams, regu_titles, regu_matches, home_advtg):
+        TrueSkillEvaluator.__init__(self, eval_teams, regu_titles, regu_matches, home_advtg)
+        self.adjust_overtime()
+
+    def adjust_overtime(self):
+        for match in self.regular_matches:
+            numot = match[self.regular_titles.index('Numot')]
+            if numot > 0:
+                match[self.regular_titles.index('Wscore')] = match[self.regular_titles.index('Lscore')]
 
 import csv
 start_season = 2005
@@ -72,8 +96,8 @@ with open('dataset/RegularSeasonCompactResults.csv', newline = '') as csvfile:
         row[regular_titles.index('Season')] = int(row[regular_titles.index('Season')])
         season = row[regular_titles.index('Season')]
         if start_season <= season <= end_season:
-            row[regular_titles.index('Wscore')] = int(row[regular_titles.index('Wscore')])
-            row[regular_titles.index('Lscore')] = int(row[regular_titles.index('Lscore')])
+            row[regular_titles.index('Wscore')] = float(row[regular_titles.index('Wscore')])
+            row[regular_titles.index('Lscore')] = float(row[regular_titles.index('Lscore')])
             row[regular_titles.index('Numot')] = int(row[regular_titles.index('Numot')])
             regular_matches.append(row)
 
@@ -88,12 +112,14 @@ for recd in predict_matches[1:]:
 
 from copy import deepcopy
 # Home advantage: deduct x scores from a home-winner
-max_home_adv = 6
-for home_adv in range (0, max_home_adv):
-    evaluator = TrueSkillEvaluator(teams, regular_titles, deepcopy(regular_matches), home_adv)
+max_home_adv = 6.0
+home_adv = 0.0
+while home_adv < max_home_adv:
+    evaluator = NoOvertime(teams, regular_titles, deepcopy(regular_matches), home_adv)
     probs = evaluator.startEvaluation()
     predict_res = list(zip(predict_matches, probs))
-    with open('results/' + str(home_adv) + '.csv', 'w', newline = '') as csvfile:
+    with open('results/' + str(home_adv).replace('.', '_') + '.csv', 'w', newline = '') as csvfile:
         writer = csv.writer(csvfile, quoting = csv.QUOTE_MINIMAL)
         writer.writerows(predict_titles)
         writer.writerows(predict_res)
+    home_adv += 0.5
